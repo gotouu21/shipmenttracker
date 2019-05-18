de6a8afb299f9eb73ffbb446d477070ac33519e2
unknown <gotouu21@gmail.com>
2019-05-18 10:22:28 +0300

    first commit

---
+++ shipmenttracking/MainActivity.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+
+namespace shipmenttracking
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        ListView lst;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            lst = FindViewById<ListView>(Resource.Id.list_view);
+            lst.Adapter = new ShipAdapter(this,);
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu1, menu);
+            return true;
+        }
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if(item.ItemId==Resource.Id.action_add)
+            {
+
+            }
+            if(item.ItemId == Resource.Id.action_quit)
+            {
+
+            }
+            return true;
+        }
+    }
+}
+
